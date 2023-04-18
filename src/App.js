a91a55eed58734a31c710ffb6bb3f68157ad4df0
bryan witkowski <bwitkowski@gmail.com>
2023-04-18 11:33:39 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from "./components/Student";
+
+function App() {
+  return (
+    <div className="App">
+      <Student name="joe blow"></Student>      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Student.js
@@ -0,0 +1,7 @@
+const Student = ({ name }) => {
+    return ( 
+        <h1>Here is my student: {name}</h1>
+     );
+}
+ 
+export default Student;
